ff5ecfa732044b377e0cbdcaba8b7e6f33d3302e
JackHoward <jhoward000@live.com>
2023-03-06 13:20:57 -0600

    1.01 Initial Commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using NLog;
+
+ // See https://aka.ms/new-console-template for more information
+ string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
+ // create instance of Logger
+ var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
+ logger.Info("Program started");
